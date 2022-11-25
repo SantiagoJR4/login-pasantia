4bffc848685dd6218b97dfd21809a0ae76a911bd
santiagoRosero <santipo12@gmail.com>
2022-11-25 11:54:14 -0500

    comienzo de formacion complementaria

--- src/app/hojavida/interfaces/formacionAcademica-interface.ts
+++ src/app/hojavida/interfaces/formacionAcademica-interface.ts
@@ -24,3 +24,6 @@ export enum ModalidadAcademica{
     PDO = 'Post Doctorado'
 
 }
+export enum ModalidadComplementaria{
+    
+}
--- src/app/hojavida/pages/formacion-complementaria/formacion-complementaria.component.ts
+++ src/app/hojavida/pages/formacion-complementaria/formacion-complementaria.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { modalidad } from '../../interfaces/formacionAcademica-interface';
 
 @Component({
   selector: 'app-formacion-complementaria',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormacionComplementariaComponent implements OnInit {
 
+  modalidadComplementariaList:modalidad[]=[];
+  selectedModalidad!:modalidad;
+  
   constructor() { }
 
   ngOnInit(): void {
+    this.modalidadComplementariaList=[
+      {codigo:'CL', nombre:'Curso Presencial'},
+      {codigo:'CV', nombre:'Curso Virtual'},
+      {codigo:'DI', nombre:'Diplomado'},
+      {codigo:'SM', nombre:'Seminario'},
+      {codigo:'CG', nombre:'Congreso'},
+      {codigo:'PR', nombre:'Premio'},
+      {codigo:'RE', nombre:'Reconocimiento'},
+    ]
   }
 
 }
